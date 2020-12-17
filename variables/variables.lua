84c054aab82ae6567d3360d719768c49a30e58fb
Frank Hofmann <frank.hofmann@efho.de>
2020-12-17 07:53:58 +0100

    added defining variables and simple assignments

---
+++ variables/variables.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/lua
+
+--[[ (C) 2020 Frank Hofmann --]]
+--[[ Using variables --]]
+
+--[[ digit (integer) --]]
+
+amount = 15
+
+--[[ digit (float) --]]
+tax_5 = 1.05
+tax_16 = 1.16
+
+--[[ string --]]
+item = "penguin"
+
+print("amount:  ", amount)
+print("tax 5%:  ", tax_5)
+print("tax 16%: ", tax_16)
