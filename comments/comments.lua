5371bd5c67b57f17d55607b9c68e42845e4ccb44
Frank Hofmann <frank.hofmann@efho.de>
2020-12-17 08:05:00 +0100

    added comment types

---
+++ comments/comments.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/lua
+
+--[[ (C) 2020 Frank Hofmann --]]
+--[[ comments --]]
+
+--[[ full comment --]]
+
+amount = 15  --inline comment
+
+print("amount:  ", amount)
+
+--[[ entire block commented out
+pet = "penguin"
+age = 3
+--]]
+
+----[[ entire block activated
+pet = "penguin"
+age = 3
+--]]
+
+
