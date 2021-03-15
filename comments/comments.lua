274bb69b28ecec9d37bde4f9a28c2c6a97473ec3
Frank Hofmann <frank.hofmann@efho.de>
2021-03-15 13:32:01 +0100

    extended comments

--- comments/comments.lua
+++ comments/comments.lua
@@ -1,13 +1,19 @@
 #!/usr/bin/lua
 
---[[ (C) 2020 Frank Hofmann --]]
+--[[ (C) 2021 Frank Hofmann --]]
 --[[ comments --]]
+
+-- simple comment (inline comment)
+
+pets = {}  -- define an empty list/table
+pets = {"goldfish", "turtle", "cat"}
+print("second entry in pets:", pets[1])
 
 --[[ full comment --]]
 
-amount = 15  --inline comment
+amount = 15
 
-print("amount:  ", amount)
+print("amount: ", amount)
 
 --[[ entire block commented out
 pet = "penguin"
