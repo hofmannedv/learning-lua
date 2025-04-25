e9c77d58fedae54580cda73bc07ddd024736d821
Frank Hofmann <frank.hofmann@efho.de>
2025-04-25 15:24:06 +0200

    add validation script (bash) for Lua code using lua-check

---
+++ validation/validate-lua-file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# -----------------------------------------------------------
+# simple validation script to validate Lua code using lua-check
+#o
+# (C) 2025 Frank Hofmann, Freiburg, Germany
+# Released under GNU Public License (GPL)
+# email frank.hofmann@efho.de
+# 
+# License: GNU Public License (GPL) v.3.0 or later
+# SPDX-License-Identifier: GNU General Public License v3.0 or later
+# -----------------------------------------------------------
+#
+# check for installation of lua-check
+# check for command-line parameters
+if [[ "$#" -gt 0 ]]
+then
+    # call lua-check with all given command-line parameters
+    lua-check "$@"
+    exit "$?"
+else
+    # called without any command-line parameters
+    # return script with error 1
+    exit 1
+fi
